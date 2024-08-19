0f7e2352b5bf9bee1f5c66d0572a64ae6e05f9f1
LucasIarlori <lucas.iarlori@mvl.edu.ar>
2024-08-19 15:23:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const GOOGLE_SHEETS_URL = 'https://script.google.com/macros/s/AKfycbzPTU2YdnLTThRrzAyR7nVy0Qm1nwUqPwvvbO2SyG80VcEvtinl0vXgdbwNSQgK6Ngn/exec';
+const GOOGLE_SHEETS_URL = 'https://script.google.com/macros/s/AKfycbzwxvCpcdJVfVYJnUGviUIBU23Mw_xrtuh2YPixxhxh4zzjO7PKtGqkx5vwFzKAbHDC/exec';
 
 function enviarDatos(nombre, cantidad, fechaIngreso, fechaRetiro, retiradoPor) {
     const data = {
@@ -9,19 +9,17 @@ function enviarDatos(nombre, cantidad, fechaIngreso, fechaRetiro, retiradoPor) {
         retiradoPor: retiradoPor
     };
 
-    console.log('Enviando datos:', data); // Mensaje de depuración
+    console.log('Enviando datos:', data);
 
     fetch(GOOGLE_SHEETS_URL, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
+        mode: 'cors' // Asegúrate de que esté configurado para manejar CORS
     })
-    .then(response => {
-        console.log('Response:', response); // Mensaje de depuración
-        return response.text();
-    })
+    .then(response => response.text())
     .then(result => console.log('Success:', result))
     .catch(error => console.error('Error:', error));
 }
